f1f2c8bd54e775720e7c72aa563da9a77d3547b2
Vishal <vishal.ai23@stellamaryscoe.edu.in>
2024-11-02 10:34:15 +0530

    Update ConditionalStatements.cs

--- ConditionalStatements.cs
+++ ConditionalStatements.cs
@@ -26,5 +26,45 @@ class Program
         }
     }
 }
+------------------------------------------------------------------------------------------------------------------------------------------
+//2) Switch Statements
+using System;
+class Program
+{
+    static void Main()
+    {
+        int month = 4; // Assign month variable with the value 4.
+        switch (month) // Start switch statement to evaluate 'month' variable.
+        {
+            case 1:
+                Console.WriteLine("January");
+                break; // Ends execution of this case
+            case 2:
+                Console.WriteLine("February");
+                break;
+            case 3:
+                Console.WriteLine("March");
+                break;
+            case 4:
+                Console.WriteLine("April");
+                break;
+            default:
+                Console.WriteLine("Invalid month");
+                break; // 'default' handles any value not matched by cases above.
+        }
+    }
+}
+--------------------------------------------------------------------------------------------------------------------------------------------
+//3) Ternary Conditional Operator
+using System;
+class Program
+{
+    static void Main()
+    {
+        int score = 75; // Define integer 'score' with a value of 75,
+        string result = (score >= 50) ? "Pass" : "Fail"; // Ternary operator to check if score >= 50, then assign "Pass" else "Fail"
+        Console.WriteLine("Result: " + result); // Displays "Pass" if score >= 50; otherwise, "Fail"
+    }
+}
 
-
+-------------------------------------------------------------------------------------------------------------------------------------------
