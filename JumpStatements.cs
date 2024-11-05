2bc662cafbee95e3e9b86f2c6313bebdc0b223ad
Vishal <vishal.ai23@stellamaryscoe.edu.in>
2024-11-05 19:18:24 +0530

    Create JumpStatements.cs

---
+++ JumpStatements.cs
@@ -0,0 +1,19 @@
+
+//Break Statements
+using System;
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (i == 3)
+            {
+                Console.WriteLine("Breaking out of the loop when i is 3");
+                break;  // Exit the loop
+            }
+            Console.WriteLine("i = " + i);
+        }
+    }
+}
+----------------------------------------------------------------------------------------------
